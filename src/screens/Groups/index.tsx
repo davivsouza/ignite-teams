2cd04df594628cde36277ac3247615dacf4a38ee
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-02-28 22:00:09 +0000

    component Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 export function Groups(){
 
     const [groups, setGroups] = useState<string[]>(["Galera do Free Fire", "Amigos"])
@@ -28,7 +29,8 @@ export function Groups(){
                     <ListEmpty message="Que tal cadastrar a primeira turma?"/>
                 )}
             />
-            <GroupCard title='Turma do TCC'/>
+
+            <Button title="Criar nova turma" type="SECONDARY"/>
         </Container>
     )
 }
