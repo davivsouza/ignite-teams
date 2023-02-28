e7f59de6f51cf2659cf93742cb6add11e970c785
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-02-28 22:05:57 +0000

    changes Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,17 @@
 import { TouchableOpacityProps } from "react-native";
-import { Title, ButtonTypeStyleProps, Container } from "./styles";
+import { ButtonTypeStyleProps, Container, Title } from "./styles";
 
-interface ButtonProps extends TouchableOpacityProps {
-  title: string;
-  type?: ButtonTypeStyleProps;
+interface ButtonProps extends TouchableOpacityProps{ 
+    title: string
+    type?: ButtonTypeStyleProps
 }
 
-export function Button({ title, type = "PRIMARY", ...rest }: ButtonProps) {
-  return (
-    <Container type={type} {...rest}>
-      <Title>{title}</Title>
-    </Container>
-  );
+export function Button({title, type = 'PRIMARY', ...rest}:ButtonProps){
+    return (
+        <Container type={type} {...rest}>
+            <Title>
+                {title}
+            </Title>
+        </Container>
+    )
 }
