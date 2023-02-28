14205de3f176fd2f65d8e8be61c3c0db952c465d
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-02-28 21:43:50 +0000

    Listagem de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,7 +2,12 @@ import {Container} from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 export function Groups(){
+
+    const [groups, setGroups] = useState<string[]>(["Galera do Free Fire", "Amigos"])
+
     return(
         <Container>
             <Header/>
@@ -10,6 +15,14 @@ export function Groups(){
             <Highlight 
                 title="Turmas"
                 subtitle='Jogue com sua turma'
+            />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item: GroupCardTitle}) => (
+                    <GroupCard title={GroupCardTitle}/>
+                )}
+                
             />
             <GroupCard title='Turma do TCC'/>
         </Container>
