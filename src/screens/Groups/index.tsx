203e84ea7989a98cc3b64110936fedbf9a6cd5c3
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-02-28 21:50:00 +0000

    ListEmptyComponent

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 export function Groups(){
 
     const [groups, setGroups] = useState<string[]>(["Galera do Free Fire", "Amigos"])
@@ -22,7 +23,10 @@ export function Groups(){
                 renderItem={({item: GroupCardTitle}) => (
                     <GroupCard title={GroupCardTitle}/>
                 )}
-                
+                contentContainerStyle={groups.length === 0 && {flex: 1}}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Que tal cadastrar a primeira turma?"/>
+                )}
             />
             <GroupCard title='Turma do TCC'/>
         </Container>
