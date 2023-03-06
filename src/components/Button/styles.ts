b8efa1155d54c8bdd5ad1c238e3cd11d968ecaac
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-03-06 22:52:35 +0000

    css helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,29 +1,28 @@
-import { TouchableOpacity } from 'react-native'
-import styled from 'styled-components/native'
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
 
-
-export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY'
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
 
 interface Props {
-    type : ButtonTypeStyleProps
+  type: ButtonTypeStyleProps;
 }
 
-
 export const Container = styled(TouchableOpacity)<Props>`
-    flex:1;
-    min-height: 56px;
-    max-height: 56px;
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
 
-    background-color: ${({theme, type}) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
-    border-radius: 6px;
-    justify-content: center;
-    align-items: center;
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
 `;
 
-
 export const Title = styled.Text`
-    font-size: ${ ({theme}) => theme.FONT_SIZE.MD}px;
-    font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
-    color: ${({theme}) => theme.COLORS.WHITE};
-
-`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
+`;
--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,24 +1,24 @@
-import styled from 'styled-components/native'
-
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
-    width: 100%;
-    margin: 32px 0;
+  width: 100%;
+  margin: 32px 0;
 `;
 
-
 export const Title = styled.Text`
-    text-align: center;
-    font-size: ${({theme}) => theme.FONT_SIZE.XL}px;
-    font-family: ${({theme}) => theme.FONT_FAMILY.BOLD}px;
-    color: ${({theme}) => theme.COLORS.WHITE}px;
-
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD}px;
+    color: ${theme.COLORS.WHITE}px;
+  `}
+  text-align: center;
 `;
 
 export const Subtitle = styled.Text`
-    text-align: center;
-    font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR}px;
-    color: ${({theme}) => theme.COLORS.GRAY_300}px;
-
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR}px;
+    color: ${theme.COLORS.GRAY_300}px;
+  `}
+  text-align: center;
 `;
