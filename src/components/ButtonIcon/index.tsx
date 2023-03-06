c381d5387545f28bc67d1cb8e55f474fa89131c6
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-03-06 23:21:47 +0000

    ButtonIcon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,20 @@
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
+import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+interface ButtonIconProps extends TouchableOpacityProps {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
+}
+
+export function ButtonIcon({
+  icon,
+  type = "PRIMARY",
+  ...rest
+}: ButtonIconProps) {
+  return (
+    <Container {...rest}>
+      <Icon name={icon} type={type} />
+    </Container>
+  );
+}
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,8 +1,16 @@
-import styled from "styled-components/native"
+import styled from "styled-components/native";
 
 export const Container = styled.View`
-    flex: 1;
-    background: ${({theme}) => theme.COLORS.GRAY_600};
-    padding: 24px;
+  flex: 1;
+  background: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
+`;
 
+export const Form = styled.View`
+  width: 100%;
+  background: ${({ theme }) => theme.COLORS.GRAY_700};
+
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 6px;
 `;
