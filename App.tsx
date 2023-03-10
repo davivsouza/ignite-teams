5791b8f0cae02d6fc731e27830ffcb52ae22b1db
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-03-10 20:52:03 +0000

    estrutura de rotas com stack navigator

--- App.tsx
+++ App.tsx
@@ -1,22 +1,24 @@
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { StatusBar } from 'expo-status-bar';
-import { Groups } from '@screens/Groups';
-import {ThemeProvider} from 'styled-components'
-import { Loading } from '@components/Loading';
-import theme from './src/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { Loading } from "@components/Loading";
+import { Routes } from "src/routes";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
-  
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-        {fontsLoaded ? <Groups/> : <Loading />}
-        <StatusBar style='light'/>
+      {fontsLoaded ? <Routes /> : <Loading />}
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
-
-
